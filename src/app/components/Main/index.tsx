526ee41f23ff6f32099b3967886ff21d281f55fa
roman-oleynik <raman.alejnik@gmail.com>
2020-08-01 12:56:03 +0300

    Add 'pictures for last two weeks' functionality

--- src/app/components/Main/index.tsx
+++ src/app/components/Main/index.tsx
@@ -4,6 +4,8 @@ import { RootState } from '../../reducers';
 import { setPicture, fetchPicture } from '../../actions/pictureActions';
 import './style.scss';
 import { Link } from 'react-router-dom';
+import axios, { AxiosResponse } from 'axios';
+import { PictureModel } from '../../models';
 
 
 export function Main() {
@@ -15,6 +17,7 @@ export function Main() {
     const storedDate = localStorage.getItem("date");
     const [date, setDate] = useState(storedDate ? storedDate : new Date().toISOString().slice(0,10));
     const [error, setError] = useState("");
+    const [picturesList, setPicturesList] = useState([] as PictureModel[]);
   
     function getAPIURL(date: string) {
       return `https://api.nasa.gov/planetary/apod?api_key=TdCcOumK7lyile0FzWeLOAa7YKBRZJL4zvnCVrUE&date=${date}`;
@@ -26,20 +29,60 @@ export function Main() {
       setDate(date);
   
       const isCorrectDate = new Date(date) <= new Date();
-      
+      const isToday = new Date(date).toISOString().slice(0,10) === new Date().toISOString().slice(0,10); 
+
       if (isCorrectDate) {
         setError("");
         dispatch(fetchPicture(getAPIURL(date)));
-        localStorage.setItem("date", date);
+        if (isToday) {
+          localStorage.removeItem("date");
+        } else {
+          localStorage.setItem("date", date);
+        }
       } else {
         setError("You did choose the incorrenct date.");
         return;
       }
     };
+
+    function createDatesList() {
+      const datesArray = [];
+
+      const nowInMs = Date.now();
+      const dayInMs = 86400000;
+
+      for (let i = 0; i < 14; i++) {
+        const date = nowInMs-dayInMs*i;
+        const dateISOStr = new Date(date).toISOString().slice(0,10);
+        datesArray.push(dateISOStr);
+      }
+
+      return datesArray;
+    }
   
     useEffect(() => {
-        dispatch(fetchPicture(getAPIURL(date)));
+      dispatch(fetchPicture(getAPIURL(date)));
+
     }, [date]);
+
+    async function getPicturesForLastTwoWeeks() {
+      const pictures: PictureModel[] = [];
+      for await (let date of createDatesList()) {
+        const res: AxiosResponse = await axios.get(getAPIURL(date));
+        pictures.push(res.data);
+      }
+  
+      try {
+        setPicturesList(pictures);
+      }
+      catch (err) {
+        throw new Error("The request is failed.")
+      }
+    }
+
+    useEffect(() => {
+      getPicturesForLastTwoWeeks();
+    }, []);
   
     return (
       <div className="App">
@@ -58,6 +101,33 @@ export function Main() {
           alt="NASA picture of a day"
         />
         <Link to={`/picture/${picture.date}`}>Show more info</Link>
+        <h2>Pictures for last 2 weeks</h2>
+        <div className="Pictures-List">
+          {
+            picturesList.length 
+            ? 
+            picturesList.map((picture: PictureModel) => {
+              if (picture.media_type === "video") {
+                return (
+                  <div key={picture.date} className="Size-Wrapper">
+                    <a href={picture.url} target="_blank" className="Link-To-Video">Video</a>
+                  </div>
+                );
+              } else if (picture.media_type === "image") {
+                return (
+                  <div key={picture.date} className="Size-Wrapper">
+                    <Link className="Link-To-Picture-Info" to={`/picture/${picture.date}`}>
+                      <img className="App__Picture-Item" src={picture.url} alt="" />
+                    </Link>
+                  </div>
+                    
+                );
+              }
+            })
+            :
+            <div>Loading...</div>
+          }
+        </div>
       </div>
     );
 }
--- src/app/components/PicturePage/index.tsx
+++ src/app/components/PicturePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../reducers';
 import { fetchPictureOnPage } from '../../actions/pictureOnPageActions';
@@ -16,15 +16,25 @@ export function PicturePage(props: Props) {
     const dispatch = useDispatch();
     const picture = useSelector((state: RootState) => state.pictureOnPage);
     const date = props.match.params.date;
+    const [isLoaded, setIsLoaded] = useState(false);
 
     function getAPIURL(date: string) {
         return `https://api.nasa.gov/planetary/apod?api_key=TdCcOumK7lyile0FzWeLOAa7YKBRZJL4zvnCVrUE&date=${date}`;
     };
 
+    async function getDataAndStopLoading() {
+        await dispatch(fetchPictureOnPage(getAPIURL(date)));
+        setIsLoaded(true);
+    }
+
     useEffect(() => {
-        dispatch(fetchPictureOnPage(getAPIURL(date)));
+        getDataAndStopLoading();
     }, []);
 
+
+    if (!isLoaded) {
+        return <div>Loading...</div>
+    }
     return (
         <div className="Picture-Page">
             <h1>{picture.title}</h1>
